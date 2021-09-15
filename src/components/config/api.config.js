720c795d49dbb227eeb6fb8173b0d983c776145f
bhargavbutani053 <bhargav053.rejoice@gmail.com>
2021-09-15 11:24:43 +0530

    host changed

--- src/components/config/api.config.js
+++ src/components/config/api.config.js
@@ -1,10 +1,10 @@
 import env from "react-dotenv"
 require('dotenv').config()
 
-// const protocol = 'https';
-// const host = "3.135.244.200:8080";
-const protocol = "http";
-const host = "localhost:8000";
+const protocol = 'https';
+const host = "3.135.244.200:8000";
+// const protocol = "http";
+// const host = "localhost:8000";
 console.log("process", env.REACT_APP_SERVER_HOST)
 const hostUrl = `${protocol}://${host}/api/`;
 export const API = {
